2a57bcb12d09ef1b2b4464751e419ea32761250c
Виссарионова Анна <anna.ogneva.455@mail.ru>
2023-04-11 19:16:40 +0300

    сделала алгоритм кодировки

--- Finite State Entropy Coding/Main.cs
+++ Finite State Entropy Coding/Main.cs
@@ -11,29 +11,63 @@ namespace Finite_State_Entropy_Coding
 
         static void Main(string[] args)
         {
-            /* var dict = DefineCondForSymb(new char[] { 'A', 'B', 'C', 'D' }, new int[] { 3, 2, 1, 4 });
-             foreach (var d in dict) {
-                 foreach(var a in d.Value)
-                 {
-                     Console.Write($"{a} ");
-                 }
-                 Console.WriteLine();
-             }*/
+            
 
-            /* var arr = GenerateArr(16, 13);
-             foreach(var i in arr)
-             {
-                 Console.Write($"{i} ");
-             }*/
+            /*   var arr = GenerateArr(16, 1);
+                foreach(var i in arr)
+                {
+                    Console.Write($"{i} ");
+                }*/
 
-            var dict = DefineInputCondForSymb(new char[] { 'A', 'B', 'C', 'D', 'E' }, new int[] { 6, 1, 3, 4, 2});
-            foreach (var d in dict) {
-                foreach(var a in d.Value)
+             var dict = DefineInputCondForSymb(new char[] { 'A', 'B', 'C', 'D', 'E' }, new int[] { 6, 1, 3, 4, 2});
+            /*foreach (var e in dict) {
+                foreach(var a in e.Value)
                 {
                     Console.Write($"{a} ");
                 }
                 Console.WriteLine();
+            }*/
+           // var dict1 = DefineCondForSymb(new char[] { 'A', 'B', 'C', 'D', 'E' }, new int[] { 6, 1, 3, 4, 2 });
+           /* foreach(var t in dict1)
+            {
+                Console.Write(t.Key + " ");
+                foreach(var x in t.Value)
+                {
+                    Console.Write($"{x} ");
+                }
+            }*/
+
+            var dict2 = DefineCondCoding(new char[] { 'A', 'B', 'C', 'D', 'E' }, new int[] { 6, 1, 3, 4, 2 }, 16);
+            var d = dict2.GetValueOrDefault('A');
+            /*foreach (var a in d)
+            {
+                Console.Write($"{a} ");
             }
+            Console.WriteLine();*/
+           /* var ind = 8;
+            Console.WriteLine(dict2.GetValueOrDefault('C')[ind-1]);*/
+
+            /*  var arr = CreateCoding(8);
+              foreach(var s in arr)
+              {
+                  Console.WriteLine(s);
+              }*/
+
+            var s = "ww";
+            //Console.WriteLine(s[2..].Equals(""));
+
+            Console.WriteLine(CodeString(4, "ABEDA", dict2, dict));
+        }
+
+
+        static string CodeString(int q, string s, Dictionary<char, string[]> encoding, Dictionary<char, int[]> q_table)
+        {
+            if (s.Equals(""))
+            {
+                return "";
+            }
+            
+            return encoding.GetValueOrDefault(s[0])[q - 1] + CodeString(q_table.GetValueOrDefault(s[0])[q-1], s[1..] ?? "", encoding, q_table);
         }
 
 
@@ -49,6 +83,7 @@ namespace Finite_State_Entropy_Coding
             return dict;
         }
 
+        //создает словарь вида "символ - массив из чисел", где каждое число - выходное состояние, каждая позиция числа - входное состояние
         static Dictionary<char, int[]> DefineInputCondForSymb(char[] arr, int[] prob)
         {
             var dict = new Dictionary<char, int[]>();
@@ -79,6 +114,82 @@ namespace Finite_State_Entropy_Coding
                 start_num += prob[i];
             }
             return dict;
+        }
+
+        //создает кодировку - словарь вида "символ - массив из строк", где каждая строка - двоичный код символа (который зависит от входного состояния), каждая позиция числа - входное состояние
+        static Dictionary<char, string[]> DefineCondCoding(char[] arr, int[] prob, int total)
+        {
+            var res = new Dictionary<char, string[]>();
+            for (var i = 0; i < arr.Length; i++)
+            {
+               // Console.Write(arr[i] + ": ");
+                //Console.WriteLine($"{e.Value.Length}");
+                var coding = new string[0];
+                var totals = GenerateArr(total, prob[i]);
+
+                for (var j = 0; j < prob[i]; j++)
+                {
+                    foreach (var s in CreateCoding(totals[j]))
+                    {
+                        coding = coding.Append(s).ToArray();
+                        //Console.WriteLine(s);
+                    }
+                }
+
+                res.Add(arr[i], coding);
+            }
+            /*foreach(var e in arr)
+            {
+                Console.Write(e + ": ");
+                //Console.WriteLine($"{e.Value.Length}");
+                var coding = new string[0];
+                for(var i = 0; i < prob[]; i++)
+                {
+                    foreach (var s in CreateCoding(e.Value[i]))
+                    {
+                        coding = coding.Append(s).ToArray();
+                        //Console.WriteLine(s);
+                    }
+                }
+                
+                res.Add(e.Key, coding);
+            }*/
+            return res;
+        }
+
+
+       /* static string CreateCoding(int total, int pos)
+        {
+            var s = pos > 0 ? "" : "0";
+            var n = pos - 1;
+            while (n > 0)
+            {
+                s = s + (n % 2);
+                n /= 2;
+            }
+            return s;
+        }*/
+
+        static string[] CreateCoding(int total)
+        {
+            var res = new string[total];
+            for(var i = 0; i < total; i++)
+            {
+                var s = "";
+                var n = i;
+                while (n > 0)
+                {
+                    s = (n % 2) + s;
+                    n /= 2;
+                }
+                
+                while(s.Length < Math.Round(Math.Floor(Math.Log2(total))))
+                {
+                    s = "0" + s;
+                }
+                res[i] = s;
+            }
+            return res;
         }
 
 
